0773e193ad0d9d590e7b59b09f222c59e8a0c120
Tolobekov Ibrakhim <74904717+tolobekov@users.noreply.github.com>
2025-07-11 13:06:15 +0200

    Update main.cpp

--- IboMicroMouse/src/main.cpp
+++ IboMicroMouse/src/main.cpp
@@ -2,6 +2,7 @@
 #include <black_box.h>
 #include <FS_MX1508.h>
 #include <micromouse.h>
+#include <PID_v1.h>
 
 unsigned long previous;
 bool pin_state=true;
@@ -12,7 +13,18 @@ uint16_t sfr;
 MX1508 motor_left(MM::PINS::MOTOR_1_A, MM::PINS::MOTOR_1_B);
 MX1508 motor_right(MM::PINS::MOTOR_2_B, MM::PINS::MOTOR_2_A);
 
+double setpoint=0.0;
+double input;
+double output;
+ double Kp=0.01;
+ double Ki=0.0;
+ double Kd=0.0;
+ PID steering_pid(&input, &output, &setpoint, Kp,Ki,Kd, DIRECT);
+
 void setup() {
+    steering_pid.SetMode(AUTOMATIC);
+    steering_pid.SetSampleTime(10);
+    steering_pid.SetOutputLimits(-50,50);
 pinMode(1, OUTPUT);
 pinMode(2, OUTPUT);
 pinMode(MM::PINS::MOTOR_DRV_EN, OUTPUT);
@@ -30,11 +42,14 @@ Serial.begin();
 }
 
 void loop() {
-
+input = sl-sr;
+steering_pid.Compute();
+motor_left.motorGoP(30-output);
+motor_right.motorGoP(30+output);
 sfl= analogRead(MM::PINS::IR_1);
 sl= analogRead(MM::PINS::IR_2);
 sr=analogRead(MM::PINS::IR_3);
 sfr= analogRead(MM::PINS::IR_4);
 Serial.printf("sl: %d, sfl: %d, sr: %d, sfr: %d\n", sl,sfl,sr,sfr);
-delay(500);
+
 }
